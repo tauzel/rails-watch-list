2fa635029c59adfd404aa8ad19ca86b090719cdd
Thierry AUZEL <thierry.auzel@gmail.com>
2021-08-26 18:15:40 +0200

    created seed with LW movie API

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,34 @@
 class ListsController < ApplicationController
+  before_action :fetch_list, only: [:show, :destroy]
+
+  def index
+    @lists = List.all
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    @list.save!
+
+    redirect_to lists_path
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+  private
+
+  def fetch_list
+    @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'open-uri'
+require 'rest-client'
+require 'faker'
+require 'json'
+
+puts "---------------------------------------------------------------"
+puts "-- Retrieving stuff from The Movie Db API ... -----------------"
+puts "---------------------------------------------------------------"
+
+url = "http://tmdb.lewagon.com/movie/top_rated"
+img_base_url = "https://image.tmdb.org/t/p/w500"
+
+response = RestClient.get(url)
+top_movies = JSON.parse(response)["results"].first(10)
+top_movies.each do |movie_hash|
+  movie = Movie.new({
+    title: movie_hash["original_title"],
+    overview: movie_hash["overview"],
+    poster_url: "#{img_base_url}#{movie_hash["poster_path"]}",
+    rating: movie_hash["vote_average"],
+  })
+  p movie
+  p movie.save
+  movie.save!
+end
+
+puts "---------------------------------------------------------------"
+puts "-- Finished! over to you Boss ---------------------------------"
+puts "---------------------------------------------------------------"
