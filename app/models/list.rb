d4b5320229471d0e8bda5aa335c5dc17c5536181
Thierry AUZEL <thierry.auzel@gmail.com>
2021-08-29 19:37:47 +0200

    added image upload logics for Lists

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,8 @@
 class List < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
+  # Allows users to post images through Cloudinary
+  has_one_attached :user_img
 
   validates :name, uniqueness: true, presence: true
 end
